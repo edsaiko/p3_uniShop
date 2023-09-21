6b77db00dc5e6b752ba2aebb025184bac29ec9e3
Ed <749490770@qq.com>
2023-09-21 17:29:52 +0800

    feat:home finished.

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    fenbao
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
